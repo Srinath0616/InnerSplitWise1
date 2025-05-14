4e885c773226cb2ef3a911b1b2cc95c7e45bdc87
Srinath0616 <132807179+Srinath0616@users.noreply.github.com>
2025-05-14 17:40:02 +0530

    Add files via upload

---
+++ SplitWise.java
@@ -0,0 +1,24 @@
+import java.util.*;
+// import 
+
+
+public class SplitWise {
+    public static void main(String[] args) {
+        
+        Scanner sc =new Scanner(System.in);
+        System.out.println("------  SpliWise -----");
+        System.out.println("Select Members to Split the amount");
+        System.out.println("1 - Srinath \n 2 - Jagan Bhai \n 3 - Karthik ");
+
+        int choice;
+        choice = sc.nextInt(); 
+        switch (choice) {
+            case 1:
+                // Contacts();
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
